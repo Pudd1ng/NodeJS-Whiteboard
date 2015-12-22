317b9e68a0e0165d93606e2bf853e2f065b9ba11
Conner Bell <Conner Bell>
2015-12-22 05:20:27 +0000

    css and html started, canvas.js finished

---
+++ assets/js/canvas.js
@@ -0,0 +1,23 @@
+﻿(function () { 
+	htmlCanvas = document.getElementById('whiteboard'),
+	context = htmlCanvas.getContext('2d');
+	initialize();
+
+	function initialize() {
+		window.addEventListener('resize', resizeCanvas, false);
+		resizeCanvas();
+	}
+	
+	function redraw() {
+		context.strokestyle = 'black';
+		context.lineWidth = '2';
+		context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+	}
+
+	function resizeCanvas() {
+		htmlCanvas.width = window.innerWidth;
+		htmlCanvas.height = window.innerHeight;
+		redraw();
+	}
+	//this function aims to resize the canvas whenever the window size is adjusted
+})();
