8c4932de4f40fb306096b26a34a42319e654f8a0
Conner Bell <Conner Bell>
2016-01-05 01:14:00 +0000

    client and whiteboard added.

---
+++ whiteboard.js
@@ -0,0 +1,21 @@
+﻿var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    nstatic = require('node-static');
+
+var fileServer = new nstatic.Server('./');
+
+app.listen(8080);
+
+function handler(request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}
+
+io.set('log level', 1);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('mousemove', function (data) {
+        socket.broadcast.emit('moving', data);
+    });
+});
